eae5a9e946827781a0a04419a777966e0bc40551
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2025-01-28 07:41:13 -0500

    adding nav

--- goose-admin/pages/index.js
+++ goose-admin/pages/index.js
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "@/components/Nav";
 
 
 export default function Home() {
@@ -7,11 +8,16 @@ export default function Home() {
   return (
 <div className="bg-blue-900 w-screen h-screen flex items-center">
 <div className="text-center w-full">
-  <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+  <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
 </div>
 </div>
   );
 }
+
+
 return (
+  <div className="bg-blue-900 min-h-screen">
+    <Nav/>
 <div> logged in {session.user.email} </div>
+</div>
 )}
